5c80959ba598ccddc5c7aed864cec8d47b04944a
renaism <zafitract@gmail.com>
2020-02-10 21:40:23 +0700

    LASTT start page

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <a href="http://temali.space/">
+            <img src="logo.png" alt="Temali Logo" />
+        </a>
+        <h3 class="font-weight-bold">Light Assessment Test</h3>
+        <hr />
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+// Light Assessment routes
+Route::get('/lastt', function() {
+    return view('lastt.start');
+});
